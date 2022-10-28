e492eb7015ba13b108d19fe395eda2490f563c37
Sunny Miglani <sunnymiglani936@gmail.com>
2022-10-28 20:55:44 +0000

    Try and install kubectl

--- Docker/generate_config.sh
+++ Docker/generate_config.sh
@@ -5,6 +5,5 @@ if [[ -z "${CLUSTER_OCID}" ]]; then
     echo "CLUSTER_OCID NOT SET!"
     exit 1
 fi
-oci ce cluster create-kubeconfig --cluster-id "$CLUSTER_OCID"
 oci ce cluster create-kubeconfig --cluster-id "$CLUSTER_OCID" --file $HOME/.kube/config --region uk-london-1 --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT
 echo "KUBECONFIG SET!"
