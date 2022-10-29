baef8bcc445c0d3088572acd7c0d512551a901f8
Sunny Miglani <sunnymiglani936@gmail.com>
2022-10-29 19:14:43 +0100

    Fix region arg in generate_config.sh

--- Docker/generate_config.sh
+++ Docker/generate_config.sh
@@ -5,7 +5,12 @@ if [[ -z "${CLUSTER_OCID}" ]]; then
     echo "CLUSTER_OCID NOT SET!"
     exit 1
 fi
-oci ce cluster create-kubeconfig --cluster-id "$CLUSTER_OCID" --file $HOME/.kube/config --region uk-london-1 --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT
+if [[ -z "${REGION}" ]]; then
+    echo "REGION NOT SET!"
+    exit 1
+fi
+
+oci ce cluster create-kubeconfig --cluster-id "$CLUSTER_OCID" --file $HOME/.kube/config --region "$REGION" --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT
 echo "KUBECONFIG SET!"
 
 
