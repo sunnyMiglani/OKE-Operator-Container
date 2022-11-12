aac38d9f8acf23fe6874067a75ee6c83ce125cac
Sunny Miglani <sunnymiglani936@gmail.com>
2022-11-12 19:25:21 +0000

    Added the container thing to the cli

--- oke-connect/my_tool.py
+++ oke-connect/my_tool.py
@@ -1,4 +1,10 @@
 import click
+import docker
+
+
+class DockerTalker:
+    def __init__(self):
+        self.client = docker.from_env()
 
 @click.group()
 def cli():
@@ -12,4 +18,14 @@ def help():
 @click.option('-n', '--name', type=str, help='Name to greet', default='World')
 def hello(name):
     click.echo(f'Hello {name}')
+
+@cli.command()
+def container():
+    setup_docker()
+
+
+# Docker section - TODO: Cleanup
+def setup_docker():
+    dockerclient = DockerTalker()
+    dockerclient.client.containers.run("ubuntu", "echo hello world")
 
