e1ed3e407869b0fd6515b0e1d25cedd7c3a0d36f
Sunny Miglani <sunnymiglani936@gmail.com>
2022-11-19 21:57:03 +0000

    Now can run docker containers
    Need to use subprocess rather than command for some smarts
    Can't use docker.from_env() since podman and docker do different things

--- oke-connect/tool.py
+++ oke-connect/tool.py
@@ -1,6 +1,7 @@
 import click
 import docker
-
+import command
+import subprocess
 
 class DockerTalker:
     def __init__(self):
@@ -27,4 +28,20 @@ def container():
 def setup_docker():
     dockerclient = DockerTalker()
     dockerclient.client.containers.run("ubuntu", "echo hello world")
+
+@cli.command()
+def shell():
+    run_shell()
+
+def run_shell():
+
+    print("Killing running oke-operator-tool container if any")
+    command.run(["podman", "kill", "oke-operator-tool"])
+
+    
+    print("Running oke-operator-tool container with the given cluster and region")
+    command.run(["podman","run","-d", "-t", "--name","oke-operator-tool", "--env","CLUSTER_OCID=ocid1.cluster.oc1.uk-london-1.aaaaaaaaao5d3nonea7h7krgyosec7afcstznbptf54scrmfvcomrxfpu3da", "--env", "REGION=uk-london-1", "localhost/operator-tool:latest"])
+
+    print("You can now run \"docker attach oke-operator-tool\" to attach to the running container")
+
 
