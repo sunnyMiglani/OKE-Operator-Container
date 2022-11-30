4491826b9b29caeafd49cea4bfa9423ae31c2785
Sunny Miglani <sunnymiglani936@gmail.com>
2022-11-30 19:13:18 +0000

    Use Shell rather than docker SDK
    Using shell is preferred, because the Podman SDK isn't working and
    it seems like it may not allow docker.run anyway

--- oke-connect/tool.py
+++ oke-connect/tool.py
@@ -1,11 +1,6 @@
 import click
-import docker
 import command
 import subprocess
-
-class DockerTalker:
-    def __init__(self):
-        self.client = docker.from_env()
 
 @click.group()
 def cli():
@@ -21,27 +16,17 @@ def hello(name):
     click.echo(f'Hello {name}')
 
 @cli.command()
-def container():
-    setup_docker()
-
-# Docker section - TODO: Cleanup
-def setup_docker():
-    dockerclient = DockerTalker()
-    dockerclient.client.containers.run("ubuntu", "echo hello world")
-
-@cli.command()
 def shell():
     run_shell()
 
 def run_shell():
+
     region = "uk-london-1"
-    print("Killing running oke-operator-tool container if any")
-    subprocess.run(["podman", "kill", "oke-operator-tool-"+region])
-
-    
+    print(f"Killing running oke-operator-tool container for {region} if any")
+    subprocess.run(["podman", "container","rm","-f", f"oke-operator-tool-{region}"])
     print("Running oke-operator-tool container with the given cluster and region")
-    subprocess.run(["podman","run","-d", "-t", "--name","oke-operator-tool-"+region, "--env","CLUSTER_OCID=ocid1.cluster.oc1.uk-london-1.aaaaaaaaao5d3nonea7h7krgyosec7afcstznbptf54scrmfvcomrxfpu3da", "--env", "REGION="+region, "localhost/operator-tool:latest"])
+    subprocess.run(["podman","run","-d", "-t", "--name",f"oke-operator-tool-{region}", "--env","CLUSTER_OCID=ocid1.cluster.oc1.uk-london-1.aaaaaaaaao5d3nonea7h7krgyosec7afcstznbptf54scrmfvcomrxfpu3da", "--env", f"REGION={region}", "localhost/operator-tool:latest"])
 
-    print("You can now run \"docker attach oke-operator-tool\" to attach to the running container")
+    print(f"You can now run \"podman attach oke-operator-tool-{region}\" to attach to the running container")
 
 
