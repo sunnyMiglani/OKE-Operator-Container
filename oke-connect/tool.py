6ba3a02b27594ccae6896650203e67ad92d18b63
Sunny Miglani <sunnymiglani936@gmail.com>
2022-12-25 16:45:41 +0000

    Added --force-init flag
    This flag triggers the termination and recreation of the container.
    This is useful if there are issues in the pre-existing container or
    we want to pick up a more up to date base container

--- oke-connect/tool.py
+++ oke-connect/tool.py
@@ -12,24 +12,20 @@ def cli():
 
 @cli.command()
 def help():
-    click.echo("Please check the readme for helpnot ") # TODO: Give more help
-
-@cli.command()
-@click.option('-n', '--name', type=str, help='Name to greet', default='World')
-def hello(name):
-    click.echo(f'Hello {name}')
+    click.echo("Please check the readme for help") # TODO: Give more help
 
 @cli.command()
 @click.option('-c', '--cluster-id', type=str, help='Which cluster to use', required=True)
 @click.option('-f','--config-file', type=click.Path(exists=True, dir_okay=False),help='Path to the config file', required=False)
-def shell(cluster_id, config_file=None):
+@click.option('--force-init', type=bool, help='Whether to force initialization of a new container', required=False)
+def shell(cluster_id, config_file=None, force_init=False):
     # We want to use the global version of this variable
     global CONFIG_FILE
     if config_file != None:
         print(config_file)
         CONFIG_FILE=config_file
         print(CONFIG_FILE)
-    runShell(CONFIG_FILE,cluster_id)
+    runShell(CONFIG_FILE,cluster_id, force_init)
 
 
 def getClusterDataFromConfig(config_file, key):
@@ -46,32 +42,58 @@ def getClusterDataFromConfig(config_file, key):
 
     return region, cluster_id
 
+def findContainer(config_key):
+    cmd = f"podman container list -a | grep oke-operator-tool-{config_key}"
+    container_exists = runCommand(cmd)
+    return container_exists
 
-def runShell(config_file,config_key):
+def restartContainer(config_key):
+    cmd = f"podman container restart oke-operator-tool-{config_key}"
+    restarted_container = runCommand(cmd)
+    return restarted_container
+
+def killContainer(config_key):
+    cmd = f"podman container rm -f oke-operator-tool-{config_key}"
+    container_killed = runCommand(cmd)
+    return container_killed
+
+def runContainer(config_key, cluster_ocid, region):
+    cmd = f"podman run -d -t --name oke-operator-tool-{config_key} --env CLUSTER_OCID={cluster_ocid} --env REGION={region} localhost/operator-tool:latest"
+    container_started = runCommand(cmd)
+    return container_started
+
+def runShell(config_file,config_key, force_init):
     print(f"Reading cluster data from config file for {config_key}")
     region, cluster_ocid = getClusterDataFromConfig(config_file, config_key)
+
     print(f"Checking whether a container is running for {config_key}")
-    cmd = f"podman container list -a | grep oke-operator-tool-{config_key}"
-    container_exists = run_cmd(cmd)
-    if container_exists:
-        print(f"Possibly found container, restarting container")
-        cmd = f"podman container restart  oke-operator-tool-{config_key}"
-        restarted_container = run_cmd(cmd)
+    container_exists = findContainer(config_key)
+    if force_init:
+        if container_exists:
+            print(f"Killing container as force init requested")
+            container_killed = killContainer(config_key)
+            if not container_killed:
+                print("Unrecoverable error: Unable to kill container, cannot proceed")
+                return
+    elif container_exists:
+        print(f"Possibly found container")
+        print(f"Restarting container")
+        restarted_container = restartContainer(config_key)
         if not restarted_container:
             print("ERROR: Unable to restart container, will kill and proceed")
-            cmd = f"podman container rm -f oke-operator-tool-{config_key}"
-            container_killed = run_cmd(cmd)
+            container_killed = killContainer(config_key)
             if not container_killed:
                 print("ERROR: Unable to recover, couldn't kill container")
-    if not container_exists:
-        cmd = f"podman run -d -t --name oke-operator-tool-{config_key} --env CLUSTER_OCID={cluster_ocid} --env REGION={region} localhost/operator-tool:latest"
-        container_started = run_cmd(cmd)
+                return
+    if not container_exists or force_init:
+        print(f"Starting container for {config_key}")
+        container_started = runContainer(config_key, cluster_ocid, region)
         if not container_started:
             print("ERROR: Unable to start container")
             return
     print(f"You can now run \"podman attach oke-operator-tool-{config_key}\" to attach to the running container")
 
-def run_cmd(cmd):
+def runCommand(cmd):
     completed_process = subprocess.run(cmd, shell=True)
     if completed_process != None:
         return completed_process.returncode == 0
